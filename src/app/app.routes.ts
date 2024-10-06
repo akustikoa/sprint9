33bc627f1a4dbd4b5e8676f8881fad08a0da093f
Xavier Prat <xavierprat78@gmail.com>
2024-10-06 16:00:53 +0200

    home i routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListDaysComponent } from './components/list-days/list-days.component';
+import { DetailsComponent } from './components/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'days', component: ListDaysComponent },
+    { path: 'details/id', component: DetailsComponent },
+    { path: '**', redirectTo: '' }
+
+];
+
