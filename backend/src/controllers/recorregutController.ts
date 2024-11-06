1350b1a6e61a395bb45f100d824eb2d9b63b3bdf
akustikoa <156797851+akustikoa@users.noreply.github.com>
2024-11-06 15:53:11 +0100

    acabant endponits de recorregut  (falta elimnar)

---
+++ backend/src/controllers/recorregutController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import Recorregut from "../models/recorregut";
+
+// Obtenir els recorreguts d'un dia 
+export const getRecorregutsByDia = async (req: Request, res: Response) => {
+    const { diaId } = req.params;
+    const recorreguts = await Recorregut.findAll({ where: { id_dia: diaId } });
+    res.json(recorreguts);
+};
+
+
+//obternir un recorregur específic 
+export const getRecorregut = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const recorregut = await Recorregut.findByPk(id);
+    if (recorregut) {
+        res.json(recorregut);
+    } else {
+        res.status(404).json({ msg: `No s'ha trobat el recorregut amb id ${id}` });
+    }
+};
+
+//crear un nou recorregut per un dia 
+export const createRecorregut = async (req: Request, res: Response) => {
+    const { body } = req;
+    try {
+        const recorregut = await Recorregut.create(body);
+        res.json(recorregut);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: `No s'ha pogut crear el recorregut` });
+    }
+};
+
+//actualitzar recorregut
+export const updateRecorregut = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req;
+    const recorregut = await Recorregut.findByPk(id);
+    if (recorregut) {
+        await recorregut.update(body);
+        res.json(recorregut);
+    } else {
+        res.status(400).json({ msg: `No s'ha pogut actualitzar el recorregut amb id ${id}` });
+    }
+};
