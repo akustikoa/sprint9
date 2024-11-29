792b6ff0d2f14071ada57219cc2171f26cdad9cd
akustikoa <156797851+akustikoa@users.noreply.github.com>
2024-11-29 13:23:57 +0100

    vista movil. login negre. maquetant contacte

--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -27,7 +27,6 @@ class Server {
             console.log(`Servidor corrent en el port ${this.port}`);
         });
     }
-
     routes() {
         this.app.use('/api/tours', tourRoutes);
         this.app.use('/api/days', diaRoutes);
@@ -54,3 +53,64 @@ class Server {
 }
 
 export default Server;
+
+
+//CODI PER XARXA LOCAL MIRAR MOVIL
+// import express, { Application } from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import tourRoutes from '../routes/tour';
+// import diaRoutes from '../routes/dia';
+// import recorregutRoutes from '../routes/recorregut';
+// import './index'; // Això inicialitza les relacions i models
+// import db from '../db/connection';
+
+// dotenv.config();
+
+// class Server {
+//     private app: Application;
+//     private port: number; // Canviat a number
+//     private host: string; // Nova variable per l'host
+
+//     constructor() {
+//         this.app = express();
+//         this.port = parseInt(process.env.PORT || '3001', 10); // Convertir a number
+//         this.host = '0.0.0.0'; // Escoltar a totes les adreces IP
+//         this.listen();
+//         this.middlewares();
+//         this.routes();
+//         this.dbConnect();
+//     }
+
+//     listen() {
+//         this.app.listen(this.port, this.host, () => {
+//             console.log(`Servidor corrent en http://${this.host}:${this.port}`);
+//         });
+//     }
+
+//     routes() {
+//         this.app.use('/api/tours', tourRoutes);
+//         this.app.use('/api/days', diaRoutes);
+//         this.app.use('/api/recorreguts', recorregutRoutes);
+//     }
+
+//     middlewares() {
+//         this.app.use(express.json());
+//         this.app.use(cors());
+//     }
+
+//     async dbConnect() {
+//         try {
+//             await db.authenticate();
+//             console.log('BD connectada');
+
+//             // Sincronitzar models (només per a desenvolupament; elimina `alter: true` a producció)
+//             await db.sync();
+//             console.log('Models sincronitzats correctament');
+//         } catch (error) {
+//             console.error('Error en la connexió de la BD o sincronització:', error);
+//         }
+//     }
+// }
+
+// export default Server;
