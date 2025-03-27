5c61391d21c1fe70be845965e3772507cdd0872c
akustikoa <156797851+akustikoa@users.noreply.github.com>
2025-03-27 12:49:40 +0100

    discover creat al front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ListDaysComponent } from './components/list-days/list-days.component';
 import { DetailsComponent } from './components/details/details.component';
-import { WeekComponent } from './components/week/week.component';
+import { DiscoverComponent } from './components/discover/discover.component';
 import { LoginComponent } from './components/login/login.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
@@ -14,12 +14,12 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'days', component: ListDaysComponent, canActivate: [AuthGuard] },
-    { path: 'detail/:id', component: DetailsComponent, canActivate: [AuthGuard] }, //'details/id'
-    { path: 'discover', component: WeekComponent, canActivate: [AuthGuard] }, //'discover/id'
+    { path: 'detail/:id', component: DetailsComponent, canActivate: [AuthGuard] },
+    { path: 'discover', component: DiscoverComponent, canActivate: [AuthGuard] },
     { path: 'contact', component: ContactComponent, canActivate: [AuthGuard] },
-    { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] }, // Panell d'administració
-    { path: 'admin-form', component: AdminFormComponent, canActivate: [AuthGuard] },// { path: 'admin/tour/new', component: AdminFormComponent }, // Formulari de creació
-    { path: 'admin-form/:id', component: AdminFormComponent, canActivate: [AuthGuard] }, // Formulari de detall/edició
+    { path: 'admin', component: AdminDashboardComponent, canActivate: [AuthGuard] },
+    { path: 'admin-form', component: AdminFormComponent, canActivate: [AuthGuard] },
+    { path: 'admin-form/:id', component: AdminFormComponent, canActivate: [AuthGuard] },
     { path: '**', redirectTo: '' }
 ];
 
