3eeb6d7298620b96ad3bae0b1d2a65606433b596
akustikoa <156797851+akustikoa@users.noreply.github.com>
2025-04-14 10:52:59 +0200

    menu hamburguesa collapse corregit

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TourService } from '../../services/tour.service';
 
+declare var bootstrap: any; //per tancar el collapse fora del menu
 
 @Component({
   selector: 'app-navbar',
@@ -16,12 +17,27 @@ export class NavbarComponent implements OnInit {
 
   constructor(private tourService: TourService) { }
 
+  ngOnInit() {
+    this.isMobile = window.innerWidth < 992;
+  }
+
   @HostListener('window:resize', [])
   onResize(): void {
-    this.isMobile = window.innerWidth < 992; // Actualitza isMobile en redimensionar
+    this.isMobile = window.innerWidth < 992;
   }
-  ngOnInit() {
-    this.isMobile = window.innerWidth < 992; // Inicialitza isMobile
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const target = event.target as HTMLElement;
+    const navbarCollapse = document.getElementById('navbarNav');
+
+    if (
+      navbarCollapse?.classList.contains('show') &&
+      !target.closest('.navbar')
+    ) {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
+      bsCollapse.hide();
+    }
   }
 
   onLogout(): void {
